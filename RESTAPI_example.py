4d824f7c1ee1a2205c345db13ad1325a5e767ea4
hygradme <hygradme@gmail.com>
2018-09-13 08:52:47 +0900

    Update RESTAPI_example.py

--- RESTAPI_example.py
+++ RESTAPI_example.py
@@ -4,24 +4,24 @@ app = flask.Flask(__name__)
 
 @app.route("/", methods=["POST"])
 def example():
-    #返答用のjsonの基本フォーマットを作成
+    #Create json format for response
     response = {
         "success": False,
         "Content-Type": "application/json"
     }
-    #POSTRでリクエストがきているか
+    #Check whether the request method is POST 
     if flask.request.method == "POST":
-        #POSTで受けたリクエスト(json形式)に"feature1"というキーがあれば処理(feature1の値をresponse["feature1"]に、response["success"]にTrueを格納し返却)
+        #If key "feature1" exists, store the value into response.
         if flask.request.get_json().get("feature1"):
             feature1 = flask.request.get_json().get("feature1")
             response["feature1"] = feature1
 
-            #feature2というキーがあればそれも格納
+            #If key "feature2" exists, store the value into response too(optional).
             if flask.request.get_json().get("feature2"):
                 feature2=flask.request.get_json().get("feature2")
                 response["feature2"]=feature2
 
-            # APIが処理を正しく終えたことを示す
+            # Show API response is success
             response["success"] = True
 
     return flask.jsonify(response)
